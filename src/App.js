1a71c131da30029cc8b0fd58638b02d3901fb2a1
Bright-Iortsor <iortsor@gmail.com>
2022-10-10 07:24:06 +0100

    calculator functions added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [cal, setCal] = useState("");
+  const [result, setResult] = useState("");
+  const operator = ["/", "*", "-", ".", "+"];
+
+  const updateCal = (value) => {
+    if (
+      // this conditional prevents adding multiple operator sign to a single digit
+      (operator.includes(value) && cal === "") ||
+      (operator.includes(value) && operator.includes(cal.slice(-1)))
+    ) {
+      return;
+    }
+    setCal(cal + value);
+
+    if (!operator.includes(value)) {
+      setResult(eval(cal + value).toString());
+    }
+  };
+
+  //Creating a function that adds numbers 1-9
+  const createNumbers = () => {
+    const numbers = [];
+    for (let i = 1; i < 10; i++) {
+      numbers.push(
+        <button onClick={() => updateCal(i.toString())} key={i}>
+          {i}
+        </button>
+      );
+    }
+    return numbers;
+  };
+
+  const calculate = () => {
+    setCal(eval(cal).toString());
+  };
+
+  const deleteBtn = () => {
+    if (cal === "") {
+      return;
+    }
+    const value = cal.slice(0, -1);
+    setCal(value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="calculator">
+        <div className="display">
+          {result ? <span></span> : ""} &nbsp;
+          {cal || 0}
+        </div>
+
+        <div className="operators">
+          <button onClick={() => updateCal("/")}>/</button>
+          <button onClick={() => updateCal("*")}>*</button>
+          <button onClick={() => updateCal("+")}>+</button>
+          <button onClick={() => updateCal("-")}>-</button>
+          <button onClick={deleteBtn}>DEL</button>
+        </div>
+
+        <div className="numbers">
+          {/* the createNumbers function is used to avoid duplicating 10 btns for 1-9 */}
+          {createNumbers()}
+          <button onClick={() => updateCal("0")}>0</button>
+          <button onClick={() => updateCal(".")}>.</button>
+
+          <button onClick={calculate}>=</button>
+        </div>
+      </div>
+      <div className="footer">Created by @BrightIortsor &copy;2022</div>
     </div>
   );
 }
